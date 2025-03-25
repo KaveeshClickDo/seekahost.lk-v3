6c30694ead109795a21d34e138893b392d0d9e08
KHSenevirathne <khsenevirathne@gmail.com>
2025-03-25 10:14:12 +0530

    register-domain search button fixed

--- src/components/register-domain/Hero.js
+++ src/components/register-domain/Hero.js
@@ -24,7 +24,7 @@ export default function Hero () {
                             placeholder="Search for a domain..."
                             className="flex-grow outline-none text-black"
                         />
-                        <button className="bg-blue-600 text-white px-6 py-2 rounded-full hover:bg-blue-700 transition">
+                        <button className="bg-blue-600 text-white px-5 md:px-10 py-4 rounded-full hover:bg-blue-700 transition">
                             Search
                         </button>
                     </div>
