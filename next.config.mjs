0954b2c5ffaffcbb6471087f0bf2bc460be0d192
KHSenevirathne <khsenevirathne@gmail.com>
2025-03-25 17:37:41 +0530

    metadata bugfixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    metadataBase: new URL(process.env.NEXT_PUBLIC_BASE_URL)
+    // metadataBase: new URL(process.env.NEXT_PUBLIC_BASE_URL || 'https://dev.seekahost.lk')
+};
 
 export default nextConfig;
