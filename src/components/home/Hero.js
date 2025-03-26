5f47f0ef306d5e2207b544211559cb0ceb79a523
KHSenevirathne <khsenevirathne@gmail.com>
2025-03-26 21:09:35 +0530

    remove curve

--- src/components/home/Hero.js
+++ src/components/home/Hero.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function Hero() {
     return (
         <section className="relative lg:h-[700px] w-full flex items-center overflow-hidden">
-            <div className="absolute inset-0 lg:w-[72%] h-[47%] min-[420px]:h-[43%] sm:h-[39%] md:h-[42%] lg:h-[74%] xl:h-[70%] bg-gradient-to-r from-[#09407A] to-[#136CC9] rounded-br-[100px] z-2"></div>
+            <div className="absolute inset-0 lg:w-[72%] h-[47%] min-[420px]:h-[43%] sm:h-[39%] md:h-[42%] lg:h-[74%] xl:h-[70%] bg-gradient-to-r from-[#09407A] to-[#136CC9] z-2"></div>
             <div className="absolute inset-0 lg:w-[72%] h-full bg-[#0A437F] rounded-br-[100px] z-1"></div>
             <Image
                 src="/images/home/home-hero-bg.svg"
