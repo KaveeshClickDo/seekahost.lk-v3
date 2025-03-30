ece4fa047863cf22b5d7c0c772117562eed8048b
KHSenevirathne <khsenevirathne@gmail.com>
2025-03-30 23:38:17 +0530

    add video playbutton to card

--- src/components/wordpress-hosting/PeopleCards.js
+++ src/components/wordpress-hosting/PeopleCards.js
@@ -11,19 +11,22 @@ export default function PeopleCards() {
             name: "David King",
             company: "Business Owner",
             image: "/images/wordpress-hosting/people-card-1.webp",
-            comment: "Lorem ipsum dolor sit uno, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna Lorem ipsum dolor sit amet"
+            comment: "Lorem ipsum dolor sit uno, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna Lorem ipsum dolor sit amet",
+            videoUrl: "https://www.youtube.com/watch?v=hx2W4fmqw_w"
         },
         {
             name: "Sarah Juli",
             company: "Business Manager",
             image: "/images/wordpress-hosting/people-card-2.webp",
-            comment: "Lorem ipsum dolor sit duos, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna Lorem ipsum dolor sit amet"
+            comment: "Lorem ipsum dolor sit duos, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna Lorem ipsum dolor sit amet",
+            videoUrl: "https://www.youtube.com/watch?v=hx2W4fmqw_w"
         },
         {
             name: "Sarah Juli",
             company: "Business Handler",
             image: "/images/wordpress-hosting/people-card-3.webp",
-            comment: "Lorem ipsum dolor sit thres, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna Lorem ipsum dolor sit amet"
+            comment: "Lorem ipsum dolor sit thres, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna Lorem ipsum dolor sit amet",
+            videoUrl: "https://www.youtube.com/watch?v=hx2W4fmqw_w"
         }
     ];
 
@@ -46,20 +49,39 @@ export default function PeopleCards() {
                 <div className="md:w-1/3 md:mr-15">
                     <div className="w-full relative">
                         <div className="w-1/2 md:w-full relative min-w-50">
-                            {people.map((src, index) => (
+                            {people.map((person, index) => (
                                 <div
                                     key={index}
                                     className={`${index === currentSlide ? "opacity-100" : "opacity-0"
                                         } absolute top-0 left-0 w-full h-full transition-opacity duration-500 ease-in-out`}
                                     style={{ display: index === currentSlide ? "block" : "none" }}
                                 >
-                                    <Image
-                                        src={src.image}
-                                        alt={`${src.name} Image`}
-                                        width={406}
-                                        height={616}
-                                        className="w-full h-auto rounded-3xl shadow-2xl shadow-black md:ml-4"
-                                    />
+                                    <div className="relative">
+                                        <Image
+                                            src={person.image}
+                                            alt={`${person.name} Image`}
+                                            width={406}
+                                            height={616}
+                                            className="w-full h-auto rounded-3xl shadow-2xl shadow-black md:ml-4"
+                                        />
+                                        {/* Play button - centered absolutely */}
+                                        <a
+                                            href={person.videoUrl}
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                            aria-label="Watch introduction video on YouTube"
+                                            className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-transparent p-3 rounded-full cursor-pointer flex items-center justify-center z-10"
+                                        >
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 26 26"
+                                                className="md:w-20 md:h-20 w-10 h-10 transition duration-1000 fill-white opacity-60 hover:fill-[#136CC9] hover:opacity-100"
+                                            >
+                                                <polygon points="9.33 6.69 9.33 19.39 19.3 13.04 9.33 6.69" />
+                                                <path d="M26,13A13,13,0,1,1,13,0,13,13,0,0,1,26,13ZM13,2.18A10.89,10.89,0,1,0,23.84,13.06,10.89,10.89,0,0,0,13,2.18Z" />
+                                            </svg>
+                                        </a>
+                                    </div>
                                 </div>
                             ))}
                             {/* Show current slide statically to maintain layout */}
@@ -72,10 +94,8 @@ export default function PeopleCards() {
                                     className="rounded-3xl w-full h-auto shadow-2xl shadow-black md:ml-4"
                                 />
                             </div>
-
                         </div>
                     </div>
-
                 </div>
 
                 <div className="md:w-2/3 mt-6 md:mt-0 md:mb-20 px-5">
