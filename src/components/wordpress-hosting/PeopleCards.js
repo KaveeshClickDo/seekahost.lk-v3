b1baa02302832e0e15bc4d4860e3f510c726c03f
KHSenevirathne <khsenevirathne@gmail.com>
2025-03-30 15:24:13 +0530

    minor fix in people card

--- src/components/wordpress-hosting/PeopleCards.js
+++ src/components/wordpress-hosting/PeopleCards.js
@@ -1,33 +1,44 @@
 import Image from "next/image"
 export default function PeopleCards() {
+
+    const people = [
+        {
+            id: 1,
+            name: "David King",
+            company: "Business Owner",
+            image: "/images/wordpress-hosting/people-card-1.webp",
+            comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna Lorem ipsum dolor sit amet"
+        }
+    ];
+
     return (
         <>
             <section className="relative min-h-[600px]">
-            <div className="absolute top-0 left-0 w-full h-full md:h-3/4 bg-gradient-to-b from-[#09407A] to-[#136CC9]" />
-            <div className="relative z-10 flex flex-col md:flex-row items-center justify-between max-w-7xl mx-auto py-12 px-4 md:px-0 h-full">
-                <div className="md:w-1/3 md:mr-15">
-                    <div className="w-full relative">
-                        <Image
-                            src="/images/wordpress-hosting/people-card-1.webp"
-                            alt="People Card Image"
+                <div className="absolute top-0 left-0 w-full h-full md:h-3/4 bg-gradient-to-b from-[#09407A] to-[#136CC9]" />
+                <div className="relative z-10 flex flex-col md:flex-row items-center justify-between max-w-7xl mx-auto py-12 px-4 md:px-0 h-full">
+                    <div className="md:w-1/3 md:mr-15">
+                        <div className="w-full relative">
+                            <Image
+                                src={people[0].image}
+                                alt="People Card Image"
 
-                            width={406}
-                            height={616}
-                            className="rounded-3xl w-1/2 md:w-full h-auto shadow-2xl shadow-black"
-                            priority
-                        />
+                                width={406}
+                                height={616}
+                                className="rounded-3xl w-1/2 md:w-full h-auto shadow-2xl shadow-black"
+                                priority
+                            />
+                        </div>
+
                     </div>
 
-                </div>
-
-                <div className="md:w-2/3 mt-6 md:mt-0 px-5">
-                    <p className="text-white mb-4 text-xl md:text-4xl font-medium">
-                        &apos;&apos;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna Lorem ipsum dolor sit amet&apos;&apos;
-                    </p>
-                    <hr className="border-t-2 border-gray-300 m-4" />
-                    <p className="mb-2 text-lg md:text-xl text-white font-semibold">David King</p>
-                    <p className="text-white">Business Owner</p>
-                </div>
+                    <div className="md:w-2/3 mt-6 md:mt-0 px-5">
+                        <p className="text-white mb-4 text-xl md:text-4xl font-medium">
+                            &apos;&apos;{people[0].comment}&apos;&apos;
+                        </p>
+                        <hr className="border-t-2 border-gray-300 m-4" />
+                        <p className="mb-2 text-lg md:text-xl text-white font-semibold">{people[0].name}</p>
+                        <p className="text-white">{people[0].company}</p>
+                    </div>
                 </div>
             </section>
         </>
