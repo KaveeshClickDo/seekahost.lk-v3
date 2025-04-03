634293bc8ab85b335cbc321ecbd7bab21d49ac01
KHSenevirathne <khsenevirathne@gmail.com>
2025-04-03 21:02:41 +0530

    resposive fix

--- src/components/home/Hero.js
+++ src/components/home/Hero.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function Hero() {
     return (
         <section className="relative lg:h-[700px] w-full flex items-center overflow-hidden">
-            <div className="absolute inset-0 lg:w-[72%] h-[47%] min-[410px]:h-[43%] min-[571px]:h-[39%] sm:h-[39%] md:h-[42%] lg:h-[74%] xl:h-[70%] bg-gradient-to-r from-[#09407A] to-[#136CC9] z-2"></div>
+            <div className="absolute inset-0 lg:w-[72%] h-[52%] min-[352px]:h-[47%] min-[389px]:h-[43%] min-[500px]:h-[39%] sm:h-[39%] md:h-[42%] lg:h-[74%] xl:h-[70%] bg-gradient-to-r from-[#09407A] to-[#136CC9] z-2"></div>
             <div className="absolute inset-0 lg:w-[72%] h-full bg-[#0A437F] rounded-br-[100px] z-1"></div>
             <Image
                 src="/images/home/home-hero-bg.svg"
