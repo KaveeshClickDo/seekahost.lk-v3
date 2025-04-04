e8c20f5637b9dfee54b84f5d01cf895bdba32af5
KHSenevirathne <khsenevirathne@gmail.com>
2025-04-04 13:21:19 +0530

    changrd image side

--- src/components/wordpress-hosting/OrderNow-1.js
+++ src/components/wordpress-hosting/OrderNow-1.js
@@ -4,21 +4,8 @@ export default function OrderNow1() {
         <>
             <section className="flex flex-col md:flex-row items-center justify-between max-w-7xl mx-auto py-12 px-4 md:px-0">
 
-                <div className="md:w-1/2 md:mr-15">
-                    <div className="w-full relative">
-                        <Image
-                            src="/images/wordpress-hosting/order-now-1.webp"
-                            alt="Order now Image"
-                            width={609}
-                            height={486}
-                            className="rounded-3xl w-full h-auto"
-                        />
-                    </div>
-
-                </div>
-
-                <div className="md:w-1/2 mt-6 md:mt-0 px-5">
-                    <h2 className="text-2xl md:text-4xl font-bold text-[#1D276A] mb-4">
+                <div className="md:w-1/2 px-5 md:mr-15">
+                <h2 className="text-2xl md:text-4xl font-bold text-[#1D276A] mb-4">
                         Easy To Manage Via Web Hosting Control Panel
                     </h2>
                     <p className="text-[#1D216A] mb-4 md:text-xl font-medium">
@@ -30,6 +17,19 @@ export default function OrderNow1() {
                     <button className="bg-[#0066CC] text-white border border-[#0066CC] px-6 py-2 rounded-full hover:bg-blue-900 transition-colors cursor-pointer md:text-lg font-medium">
                         Order Now
                     </button>
+
+                </div>
+
+                <div className="md:w-1/2 mt-6 md:mt-0">
+                <div className="w-full relative">
+                        <Image
+                            src="/images/wordpress-hosting/order-now-1.webp"
+                            alt="Order now Image"
+                            width={609}
+                            height={486}
+                            className="rounded-3xl w-full h-auto"
+                        />
+                    </div>
 
                 </div>
             </section>
