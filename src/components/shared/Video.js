851a949f127800cbd6d34e9202b0c0cb68ad4871
KHSenevirathne <khsenevirathne@gmail.com>
2025-04-08 12:02:26 +0530

    remove blu color from hover button on videojs

--- src/components/shared/Video.js
+++ src/components/shared/Video.js
@@ -48,7 +48,7 @@ export default function Video() {
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 26 26"
-                  className="w-20 h-20 transition duration-1000 fill-white opacity-60 hover:fill-[#136CC9] hover:opacity-100"
+                  className="w-20 h-20 transition duration-1000 fill-white opacity-60 hover:opacity-100"
                 >
                   <polygon points="9.33 6.69 9.33 19.39 19.3 13.04 9.33 6.69" />
                   <path d="M26,13A13,13,0,1,1,13,0,13,13,0,0,1,26,13ZM13,2.18A10.89,10.89,0,1,0,23.84,13.06,10.89,10.89,0,0,0,13,2.18Z" />
