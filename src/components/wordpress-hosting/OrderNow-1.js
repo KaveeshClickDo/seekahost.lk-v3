45a128960042328d958ead81ba4434394e4e55a2
KHSenevirathne <khsenevirathne@gmail.com>
2025-05-02 18:11:42 +0530

    whatever

--- src/components/wordpress-hosting/OrderNow-1.js
+++ src/components/wordpress-hosting/OrderNow-1.js
@@ -5,16 +5,16 @@ export default function OrderNow1() {
             <section className="flex flex-col md:flex-row items-center justify-between max-w-7xl mx-auto py-12 px-4 md:px-0 md:my-10">
 
                 <div className="md:w-1/2 px-5 md:mr-15">
-                <h2 className="text-2xl md:text-4xl font-semibold text-[#1D276A] mb-4">
+                <h2 className="text-2xl md:text-4xl font-bold text-[#1D276A] mb-4 md:mb-7">
                         Easy To Manage Via Web Hosting Control Panel
                     </h2>
-                    <p className="text-[#1D216A] mb-4 md:text-lg font-medium">
+                    <p className="text-[#1D216A] mb-4">
                         SeekaHost offers the best and easy to use WordPress hosting control panel. The custom-built web hosting control panel for WordPress gives you a 1-click WordPress hosting solution with free SSL certificates and 1-clicka activation.
                     </p>
-                    <p className="text-[#1D216A] mb-4 md:text-lg font-medium">
+                    <p className="text-[#1D216A] mb-4">
                         You can find, register domain names, host the domain and create WordPress websites with blogs faster than ever. Bloggers top choice for WP hosting and the managed WordPress is the business owners best WP partner.
                     </p>
-                    <button className="bg-[#0066CC] text-white border border-[#0066CC] px-6 py-2 rounded-full hover:bg-blue-900 transition-colors cursor-pointer md:text-lg font-medium">
+                    <button className="bg-[#0066CC] text-white border border-[#0066CC] px-6 py-2 rounded-full hover:bg-blue-900 transition-colors cursor-pointer font-bold md:mt-7">
                         Order Now
                     </button>
 
