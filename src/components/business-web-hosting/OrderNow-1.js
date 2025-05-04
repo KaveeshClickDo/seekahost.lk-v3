f02f307fd7092281306ff8903d292a92690b2b93
KHSenevirathne <khsenevirathne@gmail.com>
2025-05-04 22:50:28 +0530

    business-web-hosting fixed

--- src/components/business-web-hosting/OrderNow-1.js
+++ src/components/business-web-hosting/OrderNow-1.js
@@ -18,7 +18,7 @@ export default function OrderNow1() {
                 </div>
 
                 <div className="md:w-1/2 mt-6 md:mt-0 px-5">
-                    <h2 className="text-2xl md:text-4xl font-bold text-[#1D276A] md-4 md:mb-7">
+                    <h2 className="text-2xl md:text-4xl font-bold text-[#1D276A] md-4 md:mb-8">
                         #1 Sri Lankans Business Web Hosting Choice
                     </h2>
                     <p className="text-[#1D216A] mb-4">
@@ -27,7 +27,7 @@ export default function OrderNow1() {
                     <p className="text-[#1D216A] mb-4">
                         All our business hosting plans come with a 99.99% network uptime guarantee. SeekaHost Business Web Hosting packages are for your business to give the best performance it needs.
                     </p>
-                    <button className="bg-[#0066CC] text-white border border-[#0066CC] px-6 py-2 rounded-full hover:bg-blue-900 transition-colors cursor-pointer font-bold md:mt-7">
+                    <button className="bg-[#0066CC] text-white border border-[#0066CC] px-6 py-2 rounded-full hover:bg-blue-900 transition-colors cursor-pointer font-bold md:mt-8">
                         Order Now
                     </button>
 
