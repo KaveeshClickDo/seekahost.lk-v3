e410f89632b21630b244be7b2ae4fa65def2499a
KHSenevirathne <khsenevirathne@gmail.com>
2025-05-04 20:41:29 +0530

    register-domain fixed

--- src/components/register-domain/OrderNow.js
+++ src/components/register-domain/OrderNow.js
@@ -5,7 +5,7 @@ export default function OrderNow() {
             <section className="flex flex-col md:flex-row items-center justify-between max-w-7xl mx-auto py-12 px-4 md:px-0 md:my-10">
 
                 <div className="md:w-1/2 px-5 md:mr-15">
-                    <h2 className="text-2xl md:text-4xl font-bold text-[#1D276A] mb-4 md:mb-7">
+                    <h2 className="text-2xl md:text-4xl font-bold text-[#1D276A] mb-4 md:mb-8">
                         Finding a domain name
                     </h2>
                     <p className="text-[#1D216A] mb-4">
@@ -17,7 +17,7 @@ export default function OrderNow() {
                     <p className="text-[#1D216A] mb-4">
                         We can check what domain name will be best for you. Once you get online by registering the domain, you get hosting packages that will help your site to be hosted. We do recommend our WordPress hosting for you domains to be hosted.
                     </p>
-                    <button className="bg-[#0066CC] text-white border border-[#0066CC] px-6 py-2 rounded-full hover:bg-blue-900 transition-colors cursor-pointer font-bold md:mt-7">
+                    <button className="bg-[#0066CC] text-white border border-[#0066CC] px-6 py-2 rounded-full hover:bg-blue-900 transition-colors cursor-pointer font-bold md:mt-8">
                         Get Live Chat Support
                     </button>
 
