1c033970658995457a2c692358a55921aa7fe62e
KHSenevirathne <khsenevirathne@gmail.com>
2025-05-08 12:51:00 +0530

    feat: Add homepage hero extention prices array

---
+++ src/data/homeHeroExtnPrices.js
@@ -0,0 +1,7 @@
+export const domainPrices = [
+    { extension: '.com', price: 'Rs1976' },
+    { extension: '.co.uk', price: 'Rs1511' },
+    { extension: '.net', price: 'Rs2441' },
+    { extension: '.org', price: 'Rs2209' },
+  ];
+  
