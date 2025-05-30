2bf5fa1d4566ce8595fc0693ad3f92f264142bed
KHSenevirathne <khsenevirathne@gmail.com>
2025-05-30 21:24:33 +0530

    fix: block render error handled

--- src/components/blogs/BlocksRendererClient.js
+++ src/components/blogs/BlocksRendererClient.js
@@ -1,37 +1,22 @@
 'use client';
 
-import { notFound } from 'next/navigation';
-import React, { lazy, Suspense, useState, useEffect } from 'react';
+import NotFound from '@/app/not-found';
+import React from 'react';
+
+let BlocksRenderer = null;
+
+try {
+  BlocksRenderer = require('@strapi/blocks-react-renderer').BlocksRenderer;
+} catch (error) {
+  console.error('BlocksRenderer module not found:', error);
+  BlocksRenderer = null;
+  <NotFound />
+}
 
 export default function BlocksRendererClient({ content, ...props }) {
-  const [BlocksRenderer, setBlocksRenderer] = useState(null);
-
-  useEffect(() => {
-    let isMounted = true;
-
-    import('@strapi/blocks-react-renderer')
-      .then((module) => {
-        if (isMounted) {
-          setBlocksRenderer(() => module.BlocksRenderer);
-        }
-      })
-      .catch((error) => {
-        console.error('Failed to load BlocksRenderer:', error);
-        <notFound />
-      });
-
-    return () => {
-      isMounted = false;
-    };
-  }, []);
-
   if (!BlocksRenderer) {
     return <div>Content cannot be displayed at this time.</div>;
   }
 
-  return (
-    <Suspense fallback={<div>Loading content...</div>}>
-      <BlocksRenderer content={content} {...props} />
-    </Suspense>
-  );
+  return <BlocksRenderer content={content} {...props} />;
 }
