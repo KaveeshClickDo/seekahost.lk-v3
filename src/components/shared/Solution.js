bf3cdebc2cff416f1fdd78af217cd91d2b1eb554
KHSenevirathne <khsenevirathne@gmail.com>
2025-06-12 17:17:24 +0530

    feat: change solution grid in home page

--- src/components/shared/Solution.js
+++ src/components/shared/Solution.js
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 
-export default function Solution () {
+export default function Solution() {
   return (
     <section className="py-12 md:my-10">
       <div className="max-w-7xl mx-auto px-4 md:px-0">
@@ -12,19 +12,13 @@ export default function Solution () {
           </h2>
 
         </div>
-        
 
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 justify-items-center">
 
-          <div className="bg-[#E8F3FF] rounded-lg shadow hover:shadow-lg transition-shadow max-w-lg md:max-w-xl w-full">
-            <div className="flex items-center p-8">
-              <div className="flex-1">
-                <h3 className="text-lg md:text-2xl font-bold mb-2">DDOS Protected</h3>
-                <p className="text-gray-700">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu augue eget nisl bibendum tincidunt.
-                </p>
-              </div>
-              <div className="ml-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 justify-items-center">
+
+          <div className="bg-[#F4F7FA] rounded-lg shadow hover:shadow-lg transition-shadow max-w-lg md:max-w-xl w-full">
+            <div className="flex flex-col items-start p-8 text-left">
+              <div className="mb-4">
                 <Image
                   src="/images/shared/ddos-protected.svg"
                   alt="DDOS Protected"
@@ -33,19 +27,17 @@ export default function Solution () {
                   className="w-[70px] h-[70px] md:w-[100px] md:h-[100px]"
                 />
               </div>
+              <h3 className="text-lg md:text-2xl font-bold mb-2">DDOS Protected</h3>
+              <p className="text-gray-700">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim iamLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              </p>
             </div>
           </div>
 
 
-          <div className="bg-[#F4F4F4] rounded-lg shadow hover:shadow-lg transition-shadow max-w-lg md:max-w-xl w-full">
-            <div className="flex items-center p-8">
-              <div className="flex-1">
-                <h3 className="text-lg md:text-2xl font-bold mb-2">Instant Setup</h3>
-                <p className="text-gray-700">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu augue eget nisl bibendum tincidunt.
-                </p>
-              </div>
-              <div className="ml-4">
+          <div className="bg-[#F4F7FA] rounded-lg shadow hover:shadow-lg transition-shadow max-w-lg md:max-w-xl w-full">
+            <div className="flex flex-col items-start p-8 text-left">
+              <div className="mb-4">
                 <Image
                   src="/images/shared/instant-setup.svg"
                   alt="Instant Setup"
@@ -54,19 +46,16 @@ export default function Solution () {
                   className="w-[70px] h-[70px] md:w-[100px] md:h-[100px]"
                 />
               </div>
+              <h3 className="text-lg md:text-2xl font-bold mb-2">Instant Setup</h3>
+              <p className="text-gray-700">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim iamLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              </p>
             </div>
           </div>
 
-
-          <div className="bg-[#F4F4F4] rounded-lg shadow hover:shadow-lg transition-shadow max-w-lg md:max-w-xl w-full">
-            <div className="flex items-center p-8">
-              <div className="flex-1">
-                <h3 className="text-lg md:text-2xl font-bold mb-2">24/7 Live Support</h3>
-                <p className="text-gray-700">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu augue eget nisl bibendum tincidunt.
-                </p>
-              </div>
-              <div className="ml-4">
+          <div className="bg-[#F4F7FA] rounded-lg shadow hover:shadow-lg transition-shadow max-w-lg md:max-w-xl w-full">
+            <div className="flex flex-col items-start p-8 text-left">
+              <div className="mb-4">
                 <Image
                   src="/images/shared/24-7-live-support.svg"
                   alt="24/7 Live Support"
@@ -75,19 +64,16 @@ export default function Solution () {
                   className="w-[70px] h-[70px] md:w-[100px] md:h-[100px]"
                 />
               </div>
+              <h3 className="text-lg md:text-2xl font-bold mb-2">24/7 Live Support</h3>
+              <p className="text-gray-700">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim iamLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              </p>
             </div>
           </div>
 
-
-          <div className="bg-[#E8F3FF] rounded-lg shadow hover:shadow-lg transition-shadow max-w-lg md:max-w-xl w-full">
-            <div className="flex items-center p-8">
-              <div className="flex-1">
-                <h3 className="text-lg md:text-2xl font-bold mb-2">Guaranteed Uptime</h3>
-                <p className="text-gray-700">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu augue eget nisl bibendum tincidunt.
-                </p>
-              </div>
-              <div className="ml-4">
+          <div className="bg-[#F4F7FA] rounded-lg shadow hover:shadow-lg transition-shadow max-w-lg md:max-w-xl w-full">
+            <div className="flex flex-col items-start p-8 text-left">
+              <div className="mb-4">
                 <Image
                   src="/images/shared/guaranteed-uptime.svg"
                   alt="Guaranteed Uptime"
@@ -96,19 +82,16 @@ export default function Solution () {
                   className="w-[70px] h-[70px] md:w-[100px] md:h-[100px]"
                 />
               </div>
+              <h3 className="text-lg md:text-2xl font-bold mb-2">Guaranteed Uptime</h3>
+              <p className="text-gray-700">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim iamLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              </p>
             </div>
           </div>
 
-
-          <div className="bg-[#E8F3FF] rounded-lg shadow hover:shadow-lg transition-shadow max-w-lg md:max-w-xl w-full">
-            <div className="flex items-center p-8">
-              <div className="flex-1">
-                <h3 className="text-lg md:text-2xl font-bold mb-2">Blog Management</h3>
-                <p className="text-gray-700">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu augue eget nisl bibendum tincidunt.
-                </p>
-              </div>
-              <div className="ml-4">
+          <div className="bg-[#F4F7FA] rounded-lg shadow hover:shadow-lg transition-shadow max-w-lg md:max-w-xl w-full">
+            <div className="flex flex-col items-start p-8 text-left">
+              <div className="mb-4">
                 <Image
                   src="/images/shared/blog-management.svg"
                   alt="Blog Management"
@@ -117,19 +100,16 @@ export default function Solution () {
                   className="w-[70px] h-[70px] md:w-[100px] md:h-[100px]"
                 />
               </div>
+              <h3 className="text-lg md:text-2xl font-bold mb-2">Blog Management</h3>
+              <p className="text-gray-700">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim iamLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              </p>
             </div>
           </div>
 
-
-          <div className="bg-[#F4F4F4] rounded-lg shadow hover:shadow-lg transition-shadow max-w-lg md:max-w-xl w-full">
-            <div className="flex items-center p-8">
-              <div className="flex-1">
-                <h3 className="text-lg md:text-2xl font-bold mb-2">Periodic Malware Scanner</h3>
-                <p className="text-gray-700">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu augue eget nisl bibendum tincidunt.
-                </p>
-              </div>
-              <div className="ml-4">
+          <div className="bg-[#F4F7FA] rounded-lg shadow hover:shadow-lg transition-shadow max-w-lg md:max-w-xl w-full">
+            <div className="flex flex-col items-start p-8 text-left">
+              <div className="mb-4">
                 <Image
                   src="/images/shared/periodic-malware-scanner.svg"
                   alt="Periodic Malware Scanner"
@@ -138,6 +118,10 @@ export default function Solution () {
                   className="w-[70px] h-[70px] md:w-[100px] md:h-[100px]"
                 />
               </div>
+              <h3 className="text-lg md:text-2xl font-bold mb-2">Periodic Malware Scanner</h3>
+              <p className="text-gray-700">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim iamLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              </p>
             </div>
           </div>
         </div>
