26c628322317048ab0f2eda3b7b5ffb2f890a072
KaveeshClickDo <kaveesh@clickdo.co.uk>
2025-10-10 07:42:56 +0530

    feat: Add google analytics in Nextjs Script fotmat

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -2,6 +2,7 @@ import { DM_Sans } from 'next/font/google';
 import { NextIntlClientProvider, hasLocale } from 'next-intl';
 import { notFound } from 'next/navigation';
 import { routing } from '@/i18n/routing';
+import Script from 'next/script';
 import './globals.css';
 import './ckeditor-content.css';
 import { siteSchema } from './schema';
@@ -22,13 +23,13 @@ export const metadata = {
   description: 'UK Web hosting services company offering domains and WordPress hosting packages with cheap monthly pay plans to get online. Best 24/7 support UK Host!',
 };
 
-export default async function RootLayout({ 
-  children, 
-  params 
+export default async function RootLayout({
+  children,
+  params
 }) {
   // Handle i18n locale validation
   const { locale } = await params;
-  
+
   // Ensure that the incoming `locale` is valid
   if (!hasLocale(routing.locales, locale)) {
     notFound();
@@ -38,30 +39,44 @@ export default async function RootLayout({
     <html lang={locale}>
       <head>
         {/* Add slick-carousel CSS via CDN */}
-        <link 
-          rel="stylesheet" 
+        <link
+          rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css"
           integrity="sha512-yHknP1/AwR+yx26cB1y0cjvQUMvEa2PFzt1c9LlS4pRQ5NOTZFWbhBig+X9G9eYW/8m0/4OXNx8pxJ6z57x0dw=="
           crossOrigin="anonymous"
           referrerPolicy="no-referrer"
         />
-        <link 
-          rel="stylesheet" 
+        <link
+          rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css"
           integrity="sha512-17EgCFERpgZKcm0j0fEq1YCJuyAWdz9KUtv1EjVuaOz8pDnh/0nZxmU6BBXwaaxqoi9PQXnRWqlcDB027hgv9A=="
           crossOrigin="anonymous"
           referrerPolicy="no-referrer"
         />
         {/* Clicky Analytics */}
-        <script 
-          async 
-          data-id="101280487" 
+        <script
+          async
+          data-id="101280487"
           src="//static.getclicky.com/js"
         />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(siteSchema) }}
         />
+
+        {/* ✅ Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-67F9DR9701"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-67F9DR9701');
+          `}
+        </Script>
       </head>
       <body suppressHydrationWarning className={`${dmSans.variable} antialiased`}>
         <NextIntlClientProvider>
